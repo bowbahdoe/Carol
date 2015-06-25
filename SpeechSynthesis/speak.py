1374f6f78c108489ab4f856adfef2ee1a7076c65
Sean Sanker <seanjr@baystateit.com>
2015-06-25 00:57:35 -0400

    Update speak.py
    Added support for OSX native "say" command.

--- SpeechSynthesis/speak.py
+++ SpeechSynthesis/speak.py
@@ -1,8 +1,12 @@
 #from ctypes import *
 import os
+import platform
 #cdll.LoadLibrary(os.path.abspath(__file__)[0:-9]+"/libspeak.so")
 #speak_library = CDLL(os.path.abspath(__file__)[0:-9]+"/libspeak.so")
 #speak = speak_library.speak
 def say(words):
-    command = "padsp flite -voice slt -t '" + words + "'" #kal16
+    if(platform.system() == 'Darwin'):
+        command = "say " + words
+    else:
+        command = "padsp flite -voice slt -t '" + words + "'" #kal16
     os.system(command)
